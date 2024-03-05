89907a9ace4c9a1471904cee7f9b7af64e50e4e1
SafaT06 <safast06@gmail.com>
2024-03-05 11:40:42 +0100

    Update TitleScene.java

--- src/main/java/com/github/hanyaeger/tutorial/TitleScene.java
+++ src/main/java/com/github/hanyaeger/tutorial/TitleScene.java
@@ -4,4 +4,13 @@ import com.github.hanyaeger.api.scenes.StaticScene;
 
 public class TitleScene extends StaticScene {
 
+	@Override
+	public void setupScene() {
+
+	}
+
+	@Override
+	public void setupEntities() {
+		
+	}
 }
