7c952d741e7972aaf29af3d8771e7811b413d453
SafaT06 <safast06@gmail.com>
2024-03-05 09:35:34 +0100

    + TitleScene

---
+++ src/main/java/com/github/hanyaeger/tutorial/TitleScene.java
@@ -0,0 +1,7 @@
+package com.github.hanyaeger.tutorial;
+
+import com.github.hanyaeger.api.scenes.StaticScene;
+
+public class TitleScene extends StaticScene {
+
+}
--- src/main/java/com/github/hanyaeger/tutorial/Waterworld.java
+++ src/main/java/com/github/hanyaeger/tutorial/Waterworld.java
@@ -1,4 +1,22 @@
 package com.github.hanyaeger.tutorial;
 
-public class Waterworld {
+import com.github.hanyaeger.api.Size;
+import com.github.hanyaeger.api.YaegerGame;
+
+public class Waterworld extends YaegerGame {
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+	@Override
+	public void setupGame() {
+		setGameTitle("Waterworld");
+		setSize(new Size(800, 600));
+	}
+
+	@Override
+	public void setupScenes() {
+
+	}
 }
